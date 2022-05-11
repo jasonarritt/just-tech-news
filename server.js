f942ba2666e34be30b7e7827117c2dd931e01732
Jason Arritt <jsn.arritt@gmail.com>
2022-05-11 17:46:32 -0400

    Handlebars homepage completed

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
-
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const express = require("express");
+const path = require("path");
+const routes = require("./controllers");
+const sequelize = require("./config/connection");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -9,10 +11,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname, "public")));
+
 // turn on routes
 app.use(routes);
 
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
